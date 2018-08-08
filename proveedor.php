263aacf12b597f39194022e4ef3e81d7c2940ad1
AlejandroFuentesHernandez <41393694+AlejandroFuentesHernandez@users.noreply.github.com>
2018-08-08 13:52:50 -0600

    Update proveedor.php

--- proveedor.php
+++ proveedor.php
@@ -1,9 +1,22 @@
 <?php
 <html>
 <head>
-<meta charset='utf8_decode'>
+<meta charset='UTF-8'>
 <title></title></head>
-<body></body>
+<body>
+<form action="" method="POST">
+  <label for="nombreProveedor">Nombre:</label>
+    <input type="text" name="nombreproveedor" placeholder="Digite nombre proveedor">
+    <label for="tipodempresa">Tipo de empresa:</label>
+    <input type="text" name="tipodempresa">
+      <label for="representante">Representante:</label>
+      <input type="text" name="representante">
+        <label for="contactotelef">Numero del contacto:</label>
+        <input type="text" name="contactotelef">
+          <input type="submit" name="ingresar" value="Ingresar">
+        
+  </form>
+</body>
 
 </html>
 ?>
